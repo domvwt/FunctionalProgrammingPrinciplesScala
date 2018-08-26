612e728ddb301eaeb1fe0d04e8c2d61492a8dcfc
domvwt <dominic.thorn@gmail.com>
2018-08-26 17:01:33 +0100

    Completed filtering and sorting of Google and Apple tweets

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -209,14 +209,14 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet = TweetReader.allTweets.filter(x => google.exists(x.text.contains))
+  lazy val appleTweets: TweetSet = TweetReader.allTweets.filter(x => apple.exists(x.text.contains))
   
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-     lazy val trending: TweetList = ???
+     lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
   }
 
 object Main extends App {
